24c3e8764bdd04012e719a008ee80b214cd79ac8
amitsavani-45 <apsavani4518@gmail.com>
2026-02-03 12:03:04 +0530

    bakcned

--- backend/schoolform/serializers.py
+++ backend/schoolform/serializers.py
@@ -4,8 +4,8 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['id', 'name', 'age', 'student_class', 'school', 'subject', 'score', 'result', 'created_at']
-        read_only_fields = ['id', 'result', 'created_at']
+        fields = ['id', 'name', 'age', 'student_class', 'school', 'subject', 'score', 'result']
+        read_only_fields = ['id', 'result']
     
     def validate_age(self, value):
         if value < 5 or value > 100:
